0dc34fabdc757033404fe525222c2a7c8e5ca5f7
Arya Karnik <aryakarnik4@gmail.com>
2020-07-19 16:33:37 -0500

    Completed Exercises 1.5 and 1.6

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+bounce_start = 100
+bounce_height = bounce_start * .6
+
+while bounce_height > 0.1:
+    print(round(bounce_height, 4))
+    bounce_height = bounce_height * .6
+
+print(round(bounce_height, 4))
